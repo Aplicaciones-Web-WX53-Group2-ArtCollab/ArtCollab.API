6fa2f5a5fcdb512f55cc36b2f18dfbe5ec83b307
Mathias Jave Diaz <u202212721@upc.edu.pe>
2024-05-25 21:19:18 -0500

    refactor(monetization): refactor aggregates class into the aggregates folder

--- Artcollab/Monetization/Domain/Model/Entities/Ilustration.cs
+++ Artcollab/Monetization/Domain/Model/Entities/Ilustration.cs
@@ -1,6 +1,6 @@
 namespace artcollab.API.Monetization.Interfaces.REST;
 
-public class Ilustration
+public class Ilustration : UserFactory
 {
     public int Id { get; set; }
     public string Name { get; set; }
