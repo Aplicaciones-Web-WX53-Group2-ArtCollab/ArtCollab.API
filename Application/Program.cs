bd9d0c023dd8e6da80b612fe8711c094e856d09f
Camila Amaro <camilaamarov@yahoo.es>
2024-05-27 14:43:15 -0500

    fix(Application): Removed comment in code line
    The line of code comment that was previously commented was removed due to lack of imports.

--- Application/Program.cs
+++ Application/Program.cs
@@ -1,3 +1,5 @@
+using Domain;
+using Domain.Interfaces;
 using Infraestructure.Context;
 using Infraestructure.Interfaces;
 using Infraestructure.MySql;
@@ -13,7 +15,7 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
-//builder.Services.AddScoped(typeof(IRepositoryGeneric<>), typeof(RepositoryGeneric<>));
+builder.Services.AddScoped(typeof(IRepositoryGeneric<>), typeof(RepositoryGeneric<>));
 
 var connectionString = builder.Configuration.GetConnectionString("ArtCollabDb");
 
