298895f9aa94c84a2f63b820aa97187f8cd6cffe
Mathias Jave Diaz <u202212721@upc.edu.pe>
2024-05-27 11:27:48 -0500

    chore(Monetization): changed user root password

--- Artcollab/obj/Debug/net7.0/Application.AssemblyInfo.cs
+++ Artcollab/obj/Debug/net7.0/Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ae0b6eb4a808309b2a2f4eb618385a2f01208a08")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+68fa72e4ca741b1042060ff958d0a63ad56815c1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
