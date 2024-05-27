924e848bf714c205fed67d301e273cec81cf94d2
Camila Amaro <camilaamarov@yahoo.es>
2024-05-27 14:28:48 -0500

    feat(Domain): Added RepositoryGeneric files
    Added RepositoryGeneric class and interface.

---
+++ Domain/RepositoryGeneric.cs
@@ -0,0 +1,34 @@
+﻿using Domain.Interfaces;
+using Infraestructure.Interfaces;
+
+namespace Domain;
+
+public class RepositoryGeneric<TEntity>(IRepository<TEntity> repository) : IRepositoryGeneric<TEntity> where TEntity : class
+{
+    private readonly IRepository<TEntity> _repository = repository;
+    
+    public async Task<IEnumerable<TEntity>> GetAllAsync()
+    {
+        return await _repository.GetAllAsync();
+    }
+
+    public async Task<TEntity> GetByIdAsync(int id)
+    {
+        return await _repository.GetByIdAsync(id);
+    }
+
+    public async Task Add(TEntity entity)
+    {
+        await _repository.Add(entity);
+    }
+
+    public async Task Update(TEntity entity)
+    {
+        await _repository.Update(entity);
+    }
+
+    public async Task Delete(int id)
+    {
+        await _repository.Delete(id);
+    }
+}
