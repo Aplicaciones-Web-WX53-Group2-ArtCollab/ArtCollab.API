ce9e60768b88b37f4451dd405b5b81a02320c32e
JuanAlejandroCuadrosRodriguez <112289890+JuanAlejandroCuadrosRodriguez@users.noreply.github.com>
2024-05-26 20:36:46 -0500

    feat: Infraestructure Context and Models

---
+++ Infraestructure/Context/ArtCollabDBContext.cs
@@ -0,0 +1,36 @@
+using Infraestructure.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infraestructure.Context;
+
+public class ArtCollabDBContext : DbContext
+{
+    public ArtCollabDBContext()
+    {
+        
+    }
+    public ArtCollabDBContext(DbContextOptions<ArtCollabDBContext> options) : base(options)
+    {
+        
+    }
+    
+    public DbSet<Comment> Comments { get; set; }
+    
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        if (!optionsBuilder.IsConfigured)
+        {
+            var serverVersion = new MySqlServerVersion(new Version(8, 0, 29));
+            optionsBuilder.UseMySql("Server=localhost,3306;Uid=root;pwd=Ju!081204;Database=ArtCollab",
+                serverVersion);
+        }
+    }
+    
+protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+
+        builder.Entity<Comment>().ToTable("Comment");
+        
+    }
+}
---
+++ Infraestructure/Models/BaseModel.cs
@@ -0,0 +1,14 @@
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Infraestructure.Models;
+
+public class BaseModel
+{
+    public int Id { get; set; }
+    public int CreatedUser { get; set; }
+    public int? UpdatedUser { get; set; }
+    [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
+    public DateTime CreatedDate { get; set; } 
+    public DateTime? UpdatedDate { get; set; }
+    public Boolean IsActive { get; set; } = true;
+}
