a5225c9dfd4a6a123f49cde2b2ec00b5123c16ce
Camila Amaro <camilaamarov@yahoo.es>
2024-05-29 17:06:12 -0500

    fix(Infrastructure): Removed unnecesary builders
    Removed unnecesary builders that cause conflicts.

--- Infrastructure/Context/TemplateDBContext.cs
+++ Infrastructure/Context/TemplateDBContext.cs
@@ -19,20 +19,7 @@ namespace Infraestructure.Context
         {
             base.OnModelCreating(builder);
             
-            builder.Entity<Template>().ToTable("Templates");
-            
-            builder.Entity<Template>().HasKey(x => x.Id);
-            
-            builder.Entity<Template>().Property(x => x.Id).IsRequired().ValueGeneratedOnAdd();
-            builder.Entity<Template>().Property(x => x.Title).IsRequired();
-            builder.Entity<Template>().Property(x => x.Description).IsRequired();
-            builder.Entity<Template>().Property(x => x.Date_Publish).IsRequired();
-            builder.Entity<Template>().Property(x => x.Type).IsRequired();
-            builder.Entity<Template>().Property(x => x.ImgUrl).IsRequired();
-            builder.Entity<Template>().Property(x => x.TemplateState_Id).IsRequired();
-            builder.Entity<Template>().Property(x => x.TemplateHistory_Id).IsRequired();
-            builder.Entity<Template>().Property(x => x.Portfolio_Id).IsRequired();
-            builder.Entity<Template>().Property(x => x.Genre);
+            builder.Entity<Template>().ToTable("Template");
         }
     }
 }
