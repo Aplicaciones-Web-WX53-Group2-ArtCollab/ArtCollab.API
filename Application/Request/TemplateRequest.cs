8a9eec818d2b27ab9866b9ee9767ca43de056aa8
Camila Amaro <camilaamarov@yahoo.es>
2024-05-29 17:42:16 -0500

    feat(Application): Added Request and Response

---
+++ Application/Request/TemplateRequest.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Application.Request;
+
+public class TemplateRequest
+{
+    [Required] 
+    [MaxLength(50)]
+    public string Title { get; set; }
+    
+    [Required] 
+    [MaxLength(500)]
+    public string Description { get; set; }
+    
+    [Required] 
+    [MaxLength(120)]
+    public string Type { get; set; }
+    
+    
+    [Required] 
+    [MaxLength(120)]
+    public string ImgUrl { get; set; }
+    
+    [MaxLength(50)]
+    public string Genre { get; set; }
+}
---
+++ Application/Response/TemplateResponse.cs
@@ -0,0 +1,15 @@
+﻿namespace Application.Response;
+
+public class TemplateResponse
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public DateTime Date_Publish { get; set; }
+    public string Type { get; set; }
+    public string ImgUrl { get; set; }
+    public int TemplateStateId { get; set; }
+    public int TemplateHistoryId { get; set; }
+    public int PortfolioId { get; set; }
+    public string Genre { get; set; }
+}
