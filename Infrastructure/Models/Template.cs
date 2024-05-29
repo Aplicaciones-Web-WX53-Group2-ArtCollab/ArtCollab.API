73a6e10394b8f536be448bddcbb045df4a282597
Camila Amaro <camilaamarov@yahoo.es>
2024-05-28 19:28:09 -0500

    feat(Infrastructure): Added Models
    Added BaseModel, Template_History and TemplateState to Models section.

--- Infrastructure/Models/Template.cs
+++ Infrastructure/Models/Template.cs
@@ -1,33 +1,28 @@
-﻿namespace Infraestructure.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Infraestructure.Models
 {
-    public partial class Template
+    public partial class Template : BaseModel
     {
         public Template()
         {
-            this.Title = string.Empty;
-            this.Description = string.Empty;
-            this.Date_Publish = new DateTime();
-            this.Type = string.Empty;
-            this.ImgUrl = string.Empty;
-            this.TemplateState_Id = 0;
-            this.TemplateHistory_Id = 0;
-            this.Id = 0;
-            this.Portfolio_Id = 0;
-            this.Genre = string.Empty;
+            Date_Publish = DateTime.Now;
         }
-    }
 
-    public partial class Template
-    {
         public string Title { get; set; }
         public string Description { get; set; }
         public DateTime Date_Publish { get; set; }
         public string Type { get; set; }
         public string ImgUrl { get; set; }
-        public int TemplateState_Id { get; set; }
-        public int TemplateHistory_Id { get; set; }
-        public int Id { get; set; }
-        public int Portfolio_Id { get; set; }
+        
+        [Column("TemplateState_Id")]
+        public int TemplateStateId { get; set; }
+        
+        [Column("TemplateHistory_Id")]
+        public int TemplateHistoryId { get; set; }
+        
+        [Column("Portfolio_Id")]
+        public int PortfolioId { get; set; }
         public string Genre { get; set; }
     }
 }
---
+++ Infrastructure/Models/Template_History.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Infraestructure.Models;
+
+public partial class Template_History : BaseModel
+{
+    public Template_History()
+    {
+        ModifiedAt = DateTime.Now;
+        DeleteAt = DateTime.Now;
+    }
+    
+    [Column("Modified_At")]
+    public DateTime ModifiedAt { get; set; }
+    
+    [Column("Delete_At")]
+    public DateTime DeleteAt { get; set; }
+}
