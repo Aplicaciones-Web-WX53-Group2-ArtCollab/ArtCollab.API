3d59f4a8e46c7c553eccaf90efaa9cce32e67c8a
Frida-Dam <u202218352@upc.edu.pe>
2024-05-30 18:48:47 -0500

    feat: added basemodel

--- Infraestructure/Models/BaseModel.cs
+++ Infraestructure/Models/BaseModel.cs
@@ -7,6 +7,8 @@ public class BaseModel
     public int Id { get; set; }
     public Boolean IsActive  { get; set; }
     
+    public int CreatedUser { get; set; }
+    public int? UpdatedUser { get; set; }
     [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
     public DateTime CreatedDate  { get; set; }
     public DateTime? UpdatedDate  { get; set; }
